33aef7b6874d769d4c096241aca1eb211dd3e497
Fenri Mintardja <fenri.mintardja@salt.co.id>
2026-06-22 10:43:51 +0700

    test husky

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -33,7 +33,9 @@ export function Footer() {
           py={5}
           px={10}
         >
-          <Text size="xs">©2026 Fenri Mintardja - CRM(Solusi Klik)</Text>
+          <Text size="xs">
+            ©2026 Fenri Mintardja - CRM(Solusi Klik) TEST HUSKY
+          </Text>
           <Flex>
             <ActionIcon
               bg={"black"}
